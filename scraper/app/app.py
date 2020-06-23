c79c2adb1c10a17f4a4c8bf39960e9945a02d663
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-06-23 15:52:19 -0400

    feat: get commit stats
    - how many commits falls on which day of the week and which hour of the
      day

--- scraper/app/app.py
+++ scraper/app/app.py
@@ -1,36 +1,81 @@
 import os
 import pprint
+from datetime import datetime
 from typing import Dict, List
 
+from backports.zoneinfo import ZoneInfo
 from dotenv import load_dotenv
 from github import Github
+from github.GithubException import GithubException
+
+# logging.config.fileConfig("logging.ini", disable_existing_loggers=False)
+# logger = logging.getLogger("github_scraper")
+
+not_my_repos: List[str] = ["2019", "2020", "cusec.github.io"]
+load_dotenv(verbose=True)
+g = Github(os.getenv("GITHUB_TOKEN"))
+pp = pprint.PrettyPrinter(indent=4)
+
+
+def get_languages():
+    language_stats: Dict[str, int] = {}
+    language_percentages: Dict[str, float] = {}
+    sum_of_bytes: int = 0
+
+    for repo in g.get_user().get_repos():
+        if repo.name in not_my_repos:
+            continue
+        try:
+            for lang, bytes_of_code in repo.get_languages().items():
+                sum_of_bytes += bytes_of_code
+                language_stats[lang] = language_stats.get(lang, 0) + bytes_of_code
+        except GithubException as e:
+            pass
+            # logger.warning(str(e))
+
+    for lang, total_bytes in language_stats.items():
+        language_percentages[lang] = (total_bytes / sum_of_bytes) * 100
+
+    pp.pprint(language_percentages)
 
 
 def run():
-    language_stats_per_repo: Dict[str, Dict[str, int]] = {}
-    language_stats: Dict[str, int] = {}
-    language_percentages: Dict[str, str] = {}
-    sum_of_bytes: int = 0
-    not_my_repos: List[str] = ["2019", "2020", "cusec.github.io"]
-
-    load_dotenv(verbose=True)
-    g = Github(os.getenv("GITHUB_TOKEN"))
-
+    commit_stats_days: Dict[str, int] = {}
+    commit_stats_hours: Dict[int, int] = {}
+    sum_commits: int = 0
     for repo in g.get_user().get_repos():
+        if repo.name in not_my_repos:
+            continue
         try:
-            language_stats_per_repo[repo.name] = repo.get_languages()
-        except Exception as e:
+            commits = repo.get_commits()
+            for commit in commits:
+                sum_commits += 1
+                this_date_unaware: datetime = commit.commit.committer.date
+                this_date_aware = this_date_unaware.replace(tzinfo=ZoneInfo("UTC"))
+                local_aware = this_date_aware.astimezone(ZoneInfo("America/Montreal"))
+                this_day: str = get_day_string(local_aware)
+                commit_stats_days[this_day] = commit_stats_days.get(this_day, 0) + 1
+                commit_stats_hours[local_aware.hour] = (
+                    commit_stats_hours.get(local_aware.hour, 0) + 1
+                )
+        except GithubException as e:
             print(str(e))
 
-    for name, lang_stats in language_stats_per_repo.items():
-        if name in not_my_repos:
-            continue
-        for lang, bytes_of_code in lang_stats.items():
-            sum_of_bytes += bytes_of_code
-            language_stats[lang] = language_stats.get(lang, 0) + bytes_of_code
+    pp.pprint(commit_stats_days)
+    pp.pprint(commit_stats_hours)
 
-    for lang, total_bytes in language_stats.items():
-        language_percentages[lang] = f"{round((total_bytes / sum_of_bytes) * 100, 2)}%"
+    print(f"Total number of commits: {sum_commits}")
 
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(language_percentages)
+
+def get_day_string(date: datetime) -> str:
+    day_mapping: Dict[int, str] = {
+        0: "Monday",
+        1: "Tuesday",
+        2: "Wednesday",
+        3: "Thursday",
+        4: "Friday",
+        5: "Saturday",
+        6: "Sunday",
+    }
+
+    return day_mapping[date.weekday()]
