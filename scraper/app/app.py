1ba5464da7edfee8f09c504bf8115cd30547dc42
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-06-23 16:28:35 -0400

    fix: should only take into account commits by myself

--- scraper/app/app.py
+++ scraper/app/app.py
@@ -1,7 +1,7 @@
 import os
 import pprint
 from datetime import datetime
-from typing import Dict, List
+from typing import Dict, List, Set
 
 from backports.zoneinfo import ZoneInfo
 from dotenv import load_dotenv
@@ -17,7 +17,7 @@ g = Github(os.getenv("GITHUB_TOKEN"))
 pp = pprint.PrettyPrinter(indent=4)
 
 
-def get_languages():
+def get_languages() -> None:
     language_stats: Dict[str, int] = {}
     language_percentages: Dict[str, float] = {}
     sum_of_bytes: int = 0
@@ -30,8 +30,7 @@ def get_languages():
                 sum_of_bytes += bytes_of_code
                 language_stats[lang] = language_stats.get(lang, 0) + bytes_of_code
         except GithubException as e:
-            pass
-            # logger.warning(str(e))
+            print(str(e))
 
     for lang, total_bytes in language_stats.items():
         language_percentages[lang] = (total_bytes / sum_of_bytes) * 100
@@ -39,18 +38,24 @@ def get_languages():
     pp.pprint(language_percentages)
 
 
-def run():
+def run() -> None:
     commit_stats_days: Dict[str, int] = {}
     commit_stats_hours: Dict[int, int] = {}
     sum_commits: int = 0
+    my_names: List[str] = ["gpnn", "Gordon", "Gordon Pham-Nguyen", "gordonpn"]
+    usernames: Set[str] = set()
     for repo in g.get_user().get_repos():
         if repo.name in not_my_repos:
             continue
         try:
             commits = repo.get_commits()
             for commit in commits:
+                name: str = commit.commit.author.name
+                this_date_unaware: datetime = commit.commit.author.date
+                usernames.add(name)
+                if name not in my_names:
+                    continue
                 sum_commits += 1
-                this_date_unaware: datetime = commit.commit.committer.date
                 this_date_aware = this_date_unaware.replace(tzinfo=ZoneInfo("UTC"))
                 local_aware = this_date_aware.astimezone(ZoneInfo("America/Montreal"))
                 this_day: str = get_day_string(local_aware)
@@ -63,7 +68,7 @@ def run():
 
     pp.pprint(commit_stats_days)
     pp.pprint(commit_stats_hours)
-
+    print(f"Usernames found in commits {usernames}")
     print(f"Total number of commits: {sum_commits}")
 
 
